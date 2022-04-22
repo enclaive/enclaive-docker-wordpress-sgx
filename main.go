169318a49a05ead35cd0e134bf700208336fb90b
klassiker <6598117+klassiker@users.noreply.github.com>
2022-04-23 00:32:03 +0200

    Implement caching middleware using regular cache-fills

--- main.go
+++ main.go
@@ -5,174 +5,147 @@ package main
 import "C"
 
 import (
-    "fmt"
-    log "github.com/sirupsen/logrus"
-    "net/http"
-    "strings"
-    "os"
-    "github.com/mattn/go-pointer"
-    "unsafe"
-    "runtime"
-    "time"
-    "path/filepath"
-    "context"
+	"fmt"
+	"github.com/mattn/go-pointer"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+	"unsafe"
 )
 
 type Context struct {
-    w               http.ResponseWriter
-    r               *http.Request
-    scriptPath      string
-    dtor            []func()
-    done            chan struct{}
+	w          http.ResponseWriter
+	r          *http.Request
+	scriptPath string
+	dtor       []func()
+	done       chan struct{}
 }
 
 var PHPW = make(chan Context, 1)
 
 func phpW() {
-    runtime.LockOSThread()
+	runtime.LockOSThread()
 
-    for ctx := range PHPW {
-        phpOnce(&ctx);
-    }
+	for ctx := range PHPW {
+		phpOnce(&ctx)
+	}
 }
 
 func phpOnce(ctx *Context) {
 
-    defer func() {
+	defer func() {
 
 		if err := recover(); err != nil {
-            ctx.w.WriteHeader(500);
-            if err, ok := err.(error); ok {
-                ctx.w.Write([]byte(err.Error()))
-            }
+			ctx.w.WriteHeader(500)
+			if err, ok := err.(error); ok {
+				ctx.w.Write([]byte(err.Error()))
+			}
 		}
 
-        ctx.done <- struct{}{}
-        log.Println("cleanup");
-        for _, dtor := range ctx.dtor {
-            dtor();
-        }
-    }()
+		ctx.done <- struct{}{}
+		log.Println("cleanup")
+		for _, dtor := range ctx.dtor {
+			dtor()
+		}
+	}()
 
-    err := os.Chdir("/app/wordpress")
-    if err != nil { panic(err) }
+	err := os.Chdir("/app/wordpress")
+	if err != nil {
+		panic(err)
+	}
 
-    var c = pointer.Save(ctx);
-    defer pointer.Unref(c);
-    log.Printf("SAVE CTX PTR %x", c);
+	var c = pointer.Save(ctx)
+	defer pointer.Unref(c)
+	log.Printf("SAVE CTX PTR %x", c)
 
-    ctx.scriptPath = "." + ctx.r.URL.Path
-    //TODO more safety? since the VFS is a kernel emulation that allows escaping the mount
-    strings.ReplaceAll(ctx.scriptPath, "..", "")
-    if strings.HasPrefix(ctx.scriptPath, "/") {
-        ctx.scriptPath = "." + ctx.scriptPath
-    }
+	ctx.scriptPath = "." + ctx.r.URL.Path
+	//TODO more safety? since the VFS is a kernel emulation that allows escaping the mount
+	strings.ReplaceAll(ctx.scriptPath, "..", "")
+	if strings.HasPrefix(ctx.scriptPath, "/") {
+		ctx.scriptPath = "." + ctx.scriptPath
+	}
 
-    var script_path = C.CString(filepath.Join("/app/wordpress/", ctx.scriptPath));
-    defer C.free(unsafe.Pointer(script_path))
+	var script_path = C.CString(filepath.Join("/app/wordpress/", ctx.scriptPath))
+	defer C.free(unsafe.Pointer(script_path))
 
-    var request_method = C.CString(ctx.r.Method);
-    defer C.free(unsafe.Pointer(request_method));
+	var request_method = C.CString(ctx.r.Method)
+	defer C.free(unsafe.Pointer(request_method))
 
-    var request_uri = C.CString(ctx.r.RequestURI);
-    defer C.free(unsafe.Pointer(request_uri));
+	var request_uri = C.CString(ctx.r.RequestURI)
+	defer C.free(unsafe.Pointer(request_uri))
 
-    var query_string = C.CString(ctx.r.URL.RawQuery);
-    defer C.free(unsafe.Pointer(query_string));
+	var query_string = C.CString(ctx.r.URL.RawQuery)
+	defer C.free(unsafe.Pointer(query_string))
 
-    var content_type = C.CString(ctx.r.Header.Get("Content-Type"));
-    defer C.free(unsafe.Pointer(content_type));
+	var content_type = C.CString(ctx.r.Header.Get("Content-Type"))
+	defer C.free(unsafe.Pointer(content_type))
 
-    var content_length = C.size_t(ctx.r.ContentLength);
+	var content_length = C.size_t(ctx.r.ContentLength)
 
-    C.phpmain(
-        c,
-        script_path,
-        request_method,
-        request_uri,
-        query_string,
-        content_type,
-        content_length,
-    );
-    log.Println("phpmain should be done");
+	C.phpmain(
+		c,
+		script_path,
+		request_method,
+		request_uri,
+		query_string,
+		content_type,
+		content_length,
+	)
+	log.Println("phpmain should be done")
 }
 
 func main() {
 
-    fmt.Println("starting");
+	fmt.Println("starting")
 
-    ExtractAppZip();
+	ExtractAppZip()
 
-    //TODO spawn more workers. but php needs thread locals and i'm not confident yet they actually work correctly in gramine.
-    go phpW();
+	//TODO spawn more workers. but php needs thread locals and i'm not confident yet they actually work correctly in gramine.
+	go phpW()
 
-    fs := http.FileServer(http.Dir("/app/wordpress"))
+	fs := http.FileServer(http.Dir("/app/wordpress"))
 
-    router := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	router := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-        log.Println(r.URL);
+		log.Println(r.URL)
 
-        if strings.HasSuffix(r.URL.Path, "/") {
-            r.URL.Path += "index.php"
-        }
-        if strings.HasSuffix(r.URL.Path, ".php") {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			r.URL.Path += "index.php"
+		}
+		if strings.HasSuffix(r.URL.Path, ".php") {
 
-            //safety
-            strings.ReplaceAll(r.URL.Path, "..", "")
+			//safety
+			strings.ReplaceAll(r.URL.Path, "..", "")
 
-            var ctx = Context{
-                w: w,
-                r: r,
-                done: make(chan struct{}),
-            };
-
-            select {
-                case PHPW <- ctx:
-                    <- ctx.done
-                case <-time.After(time.Second):
-                    close(ctx.done)
-                    w.WriteHeader(503);
-                    w.Write([]byte("all enclaves busy. try again later"));
-            }
-
-            log.Println("phpmain should be done");
-
-        } else {
-            fs.ServeHTTP(w, r);
-        }
-    })
-
-    fmt.Println("listening on https://0.0.0.0:443");
-    panic(http.ListenAndServeTLS("0.0.0.0:443", "/app/server.crt", "/app/server.key", tracing()(logging()(router))))
-}
-
-type requestIDKeyType int
-var  requestIDKey requestIDKeyType = 0
-
-func tracing() func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			requestID := r.Header.Get("X-Request-Id")
-			if requestID == "" {
-				requestID = fmt.Sprintf("%d", time.Now().UnixNano())
+			var ctx = Context{
+				w:    w,
+				r:    r,
+				done: make(chan struct{}),
 			}
-			ctx := context.WithValue(r.Context(), requestIDKey, requestID)
-			w.Header().Set("X-Request-Id", requestID)
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
-}
-func logging() func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			defer func() {
-				requestID, ok := r.Context().Value(requestIDKey).(string)
-				if !ok {
-					requestID = "unknown"
-				}
-				fmt.Println("[HTTPD] FIN ", requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent(), "\n")
-			}()
-			next.ServeHTTP(w, r)
-		})
-	}
+
+			select {
+			case PHPW <- ctx:
+				<-ctx.done
+			case <-time.After(time.Second):
+				close(ctx.done)
+				w.WriteHeader(503)
+				w.Write([]byte("all enclaves busy. try again later"))
+			}
+
+			log.Println("phpmain should be done")
+
+		} else {
+			fs.ServeHTTP(w, r)
+		}
+	})
+
+	handler := tracing(logging(caching(router)))
+	go cachingRequest(handler)
+
+	fmt.Println("listening on https://0.0.0.0:443")
+	panic(http.ListenAndServeTLS("0.0.0.0:443", "/app/server.crt", "/app/server.key", handler))
 }
---
+++ middleware.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"time"
+)
+
+type requestIDKeyType int
+
+var requestIDKey requestIDKeyType = 0
+
+var requestCachingPaths = map[string]bool{
+	"/": true,
+}
+
+var requestCachingStore = map[string]requestCacheResponse{}
+
+const requestCachingTTL = 5 * time.Minute
+const requestCachingInterval = 1 * time.Minute
+
+type requestCacheResponse struct {
+	status  int
+	header  http.Header
+	body    []byte
+	expires time.Time
+}
+
+func cachingRequest(handler http.Handler) {
+	ticker := time.NewTicker(requestCachingInterval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		for path := range requestCachingPaths {
+			request, err := http.NewRequest(http.MethodGet, path, nil)
+
+			if err != nil {
+				log.Println("[cache] Interval request for", path, "failed with", err)
+				continue
+			}
+
+			handler.ServeHTTP(httptest.NewRecorder(), request)
+		}
+	}
+}
+
+// adapted from: https://github.com/victorspringer/http-cache/blob/master/cache.go
+func caching(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		respond := func(status int, header http.Header, body []byte) {
+			for k, v := range header {
+				w.Header().Set(k, strings.Join(v, ","))
+			}
+
+			w.WriteHeader(status)
+			_, err := w.Write(body)
+			if err != nil {
+				log.Println("[cache] Error writing response", err)
+				return
+			}
+		}
+
+		// only handle get without query parameters for now
+		// exclude http-auth just in case
+		if r.Method == http.MethodGet && len(r.URL.Query()) == 0 && r.URL.User.String() == "" {
+			path := r.URL.Path
+
+			log.Println("[cache] Received GET request for", path)
+
+			if stored, found := requestCachingStore[path]; found && stored.expires.After(time.Now()) {
+				log.Println("[cache] Path found in cache-store and response is not expired")
+
+				respond(stored.status, stored.header, stored.body)
+			} else if _, found = requestCachingPaths[path]; found {
+				log.Println("[cache] Path is marked for caching")
+
+				recorder := httptest.NewRecorder()
+
+				next.ServeHTTP(recorder, r)
+
+				result := recorder.Result()
+				response := recorder.Body.Bytes()
+
+				if result.StatusCode >= 200 && result.StatusCode < 400 {
+					log.Println("[cache] Request succeeded with status code", result.StatusCode, "adding to cache")
+
+					requestCachingStore[path] = requestCacheResponse{
+						status:  result.StatusCode,
+						body:    response,
+						header:  result.Header,
+						expires: time.Now().Add(requestCachingTTL),
+					}
+				} else {
+					log.Println("[cache] Request failed with status code", result.StatusCode, "not caching")
+				}
+
+				respond(result.StatusCode, result.Header, response)
+			} else {
+				log.Println("[cache] Path is not marked for caching")
+
+				next.ServeHTTP(w, r)
+			}
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+func tracing(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestID := r.Header.Get("X-Request-Id")
+		if requestID == "" {
+			requestID = fmt.Sprintf("%d", time.Now().UnixNano())
+		}
+		ctx := context.WithValue(r.Context(), requestIDKey, requestID)
+		w.Header().Set("X-Request-Id", requestID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			requestID, ok := r.Context().Value(requestIDKey).(string)
+			if !ok {
+				requestID = "unknown"
+			}
+			fmt.Println("[HTTPD] FIN ", requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
