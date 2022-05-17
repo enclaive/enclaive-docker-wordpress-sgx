c44503bfe615532e69614211bc60ec5603982573
klassiker <6598117+klassiker@users.noreply.github.com>
2022-05-17 21:59:18 +0200

    Auto-format apploader

--- webserver/apploader.go
+++ webserver/apploader.go
@@ -1,42 +1,51 @@
 package main
 
 import (
-    "archive/zip"
-    "fmt"
-    "io"
-    "os"
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
 )
 
-
 func ExtractAppZip() error {
-    err := os.Chdir("/app/")
-    if err != nil { panic(err) }
+	err := os.Chdir("/app/")
+	if err != nil {
+		panic(err)
+	}
 
-    r, err := zip.OpenReader("/app/app.zip")
-    if err != nil { panic(err) }
+	r, err := zip.OpenReader("/app/app.zip")
+	if err != nil {
+		panic(err)
+	}
 
 	for _, f := range r.File {
 
 		fmt.Println("extracting", f.Name)
 
-        fi := f.FileInfo()
-        if fi.IsDir() {
-            os.MkdirAll(f.Name, os.ModePerm)
-            continue;
-        }
+		fi := f.FileInfo()
+		if fi.IsDir() {
+			os.MkdirAll(f.Name, os.ModePerm)
+			continue
+		}
 
 		fr, err := f.Open()
-		if err != nil { panic(err) }
+		if err != nil {
+			panic(err)
+		}
 
-        fw, err := os.Create(f.Name);
-		if err != nil { panic(err) }
+		fw, err := os.Create(f.Name)
+		if err != nil {
+			panic(err)
+		}
 
 		_, err = io.Copy(fw, fr)
-		if err != nil { panic(err) }
+		if err != nil {
+			panic(err)
+		}
 
 		fr.Close()
 		fw.Close()
 	}
 
-    return nil
+	return nil
 }
